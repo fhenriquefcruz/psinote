2503c6eb223aeeca250107871abd31bb545c112f
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 12:36:57 -0400

    Create constants.js

---
+++ src/utils /constants.js
@@ -0,0 +1,2 @@
+export const EMOTIONAL_STATES = ['Excelente', 'Bom', 'Regular', 'Ruim', 'Muito Ruim'];
+export const TAGS = ['Ansiedade', 'Depressão', 'Autoestima', 'Família', 'Relacionamento', 'Trabalho', 'Luto', 'TDAH', 'Burnout', 'Outro'];
