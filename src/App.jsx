52037b0cf852210254938a87dc7420495933f9cd
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 11:24:10 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { AuthProvider } from './contexts/AuthContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import AppRoutes from './routes/AppRoutes'
+
+function App() {
+  return (
+    <AuthProvider>
+      <AppRoutes />
+      <ToastContainer position="bottom-right" />
+    </AuthProvider>
+  )
+}
+
+export default App
