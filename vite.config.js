91c770c0a0270699f57aff6edbaf4bd1f9332695
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 16:42:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/psinote/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false
+    rollupOptions: {
+      output: {
+        // Garante nomes únicos com hash
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   }
 })
