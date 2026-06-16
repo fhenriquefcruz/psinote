a817cd68898517f34627c657a8aa8a287ad69274
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 11:35:19 -0400

    Delete src/components/Card directory

--- src/components/Card/Card.jsx
+++
@@ -1,3 +0,0 @@
-export default function Card({ children }) {
-  return <div style={{ background: 'var(--bg-primary)', padding: '1rem', borderRadius: '8px', boxShadow: 'var(--card-shadow)' }}>{children}</div>;
-}
