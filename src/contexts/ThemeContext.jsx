3dc0635c4d2ef6a2b28f9fcf1f023b82ae7b8790
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 11:41:21 -0400

    Create ThemeContext.jsx

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState, useContext } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div data-theme={theme}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
