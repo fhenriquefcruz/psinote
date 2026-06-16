79990643cf3ec11d49a3961f5458faf1b2dca2bc
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-16 12:37:31 -0400

    Delete src/src directory

--- src/src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const EMOTIONAL_STATES = ['Excelente', 'Bom', 'Regular', 'Ruim', 'Muito Ruim'];
-export const TAGS = ['Ansiedade', 'Depressão', 'Autoestima', 'Família', 'Relacionamento', 'Trabalho', 'Luto', 'TDAH', 'Burnout', 'Outro'];
--- src/src/utils/formatters.js
+++
@@ -1,5 +0,0 @@
-export const formatDate = (timestamp) => {
-  if (!timestamp) return '';
-  const date = timestamp.toDate ? timestamp.toDate() : new Date(timestamp);
-  return date.toLocaleDateString('pt-BR');
-};
