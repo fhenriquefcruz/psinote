e8500735f81251b8d0d5abfceaa008cedcfbb39d
Fabio Henrique <59840567+fhenriquefcruz@users.noreply.github.com>
2026-06-17 08:47:46 -0400

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,2 @@
-// Dentro de AuthContext.jsx
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
-};
+// Re-exportação do useAuth do contexto para manter compatibilidade
+export { useAuth } from '../contexts/AuthContext';
